2fba1487896bf777e4b9cd6ab0a041c32b214227
TouchsadaJanOn <57841675+TouchsadaJanOn@users.noreply.github.com>
2022-04-23 13:42:50 -0600

    initial stuff

---
+++ Code/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to this AWESOME Cache Replacement Algorithm Assignment!!!\n");
+        System.out.print("Enter page reference pattern length: ");
+        int page_len = input.nextInt();
+        System.out.print("\nEnter number of unique pages: ");
+        int uniq_page = input.nextInt();
+        System.out.print("\nEnter number of slots: ");
+        int slots = input.nextInt();
+
+
+    }
+}
