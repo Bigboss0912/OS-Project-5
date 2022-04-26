efe38690a52dc7e86035eec9f25ba1e6ce1a6b4d
TouchsadaJanOn <57841675+TouchsadaJanOn@users.noreply.github.com>
2022-04-25 19:32:50 -0600

    exceptions and inputs

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,15 +10,47 @@ public class Main {
         Random rand = new Random();
         char page;
         int count = 0;
+        int page_len = 0;
+        int uniq_page = 0;
+        int slots = 0;
+        boolean page_len_catch = false;
+        boolean uniq_page_catch = false;
+        boolean slots_catch = false;
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to this AWESOME Cache Replacement Algorithm Assignment!!!\n");
-        System.out.print("Enter page reference pattern length: ");
-        int page_len = input.nextInt();
-        System.out.print("\nEnter number of unique pages: ");
-        int uniq_page = input.nextInt();
-        System.out.print("\nEnter number of slots: ");
-        int slots = input.nextInt();
+        System.out.println("\nWelcome to this AWESOME Cache Replacement Algorithm Assignment!!!\n");
+
+        while (page_len_catch == false) {
+                System.out.print("Enter page reference pattern length: ");
+                page_len = input.nextInt();
+                if (page_len <= 9 || page_len >= 101) {
+                    System.out.println("Please enter page reference pattern length between 10-100!!!");
+                    page_len_catch = false;
+                } else {
+                    page_len_catch = true;
+                }
+        }
+        while (uniq_page_catch == false) {
+            System.out.print("\nEnter number of unique pages: ");
+            uniq_page = input.nextInt();
+            if (uniq_page <= 1 || uniq_page >= 16) {
+                System.out.println("Please enter unique page number between 2-15!!!");
+                uniq_page_catch = false;
+            } else {
+                uniq_page_catch = true;
+            }
+        }
+        while (slots_catch == false) {
+            System.out.print("\nEnter number of slots: ");
+            slots = input.nextInt();
+            if (slots <= 1 || slots >= 11) {
+                System.out.println("Please enter slots number between 2-10!!!");
+                slots_catch = false;
+            } else {
+                slots_catch = true;
+            }
+        }
+
 
         for (page = 'A'; page <= 'Z'; page++) {
             if (count < uniq_page) {
@@ -28,6 +61,7 @@ public class Main {
                 break;
             }
         }
+
         count = uniq_page;
         for (int i = 0; i < ref_rand_list.size(); i++) {
             if (count < page_len) {
@@ -38,6 +72,9 @@ public class Main {
         final_ref_list = Arrays.asList(ref_str.split(" "));
         System.out.println("\nRef Str: " + ref_str);
 
+        //FIFO Page replacement Algorithm
+
 
     }
+
 }
